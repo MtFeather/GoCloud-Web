402811c7e5a39bc3850ce81e50b9537246c7f9d5
MtFeather <st904583114@gmail.com>
2019-07-31 11:18:01 +0800

    first commit.

--- scripts/config.sh
+++ scripts/config.sh
@@ -11,6 +11,6 @@ psql="/opt/rh/rh-postgresql10/root/usr/bin/psql -U gocloud"
 # Ovirt REST API INFO
 url="https://localhost/ovirt-engine/api"
 user="admin@internal"
-password="2727175#356"
+password="password"
 
 [ ! -d ${basedir} ] && mkdir -p ${basedir}
